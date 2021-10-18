1044d41e1e6951dfc9413258f25ee4abb06aa475
Chris Kennedy <chrisken@gmail.com>
2021-10-18 09:44:11 -0400

    move more code into R/

---
+++ R/aggregators.R
@@ -0,0 +1,8 @@
+agg_reciprocal_rank = function(rank_df,
+                               ties_method = "last") {
+  # For each feature, sum the inverse of its ranks, then invert the sum.
+  ranks = apply(rank_df, MARGIN = 1, function(row) {
+     1 / sum(row^-1)
+  })
+  return(rank(ranks, ties.method = ties_method))
+}
---
+++ R/ensemble_rank.R
@@ -0,0 +1,29 @@
+ensemble_rank =
+  function(Y, X, family,
+           # Feature ranking algorithms.
+           fn_rank = c(featrank_cor, featrank_randomForest, featrank_glm),
+           # Rank aggregation algorithm.
+           fn_agg = agg_reciprocal_rank,
+           # How many variables we want to select. Top 50% by default.
+           top_vars = ceiling(ncol(X) / 2),
+           ties_method = getOption("featrank.ties.default", "last"),
+           # Set as TRUE for debugging purposes
+           return_ranking = FALSE,
+           # Not used.
+           ...)  {
+    
+    # Run each feature ranking algorithm.
+    rank_df = do.call(cbind.data.frame,
+                      lapply(fn_rank, function(fn) fn(Y, X, family, ties_method)))
+    
+    # Calculate the aggregate ranking.
+    ranking = fn_agg(rank_df, ties_method)
+    
+    if (return_ranking) {
+      return(ranking)
+    }
+    
+    # Apply the ranking to select the top X variables.
+    whichVariable <- (ranking <= top_vars)
+    return(whichVariable)
+  }
