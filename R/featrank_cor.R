1e38161c56ca06b8752138e673a48816512a33f3
Chris Kennedy <chrisken@gmail.com>
2021-11-03 06:07:03 -0400

    featrank_cor: update to support observation weights

--- R/featrank_cor.R
+++ R/featrank_cor.R
@@ -1,12 +1,15 @@
 # Edited from SuperLearner::screen.corRank()
 #' @export
 featrank_cor =
-  function(Y, X, family, obsWeights = NULL,
+  function(Y, X, family, obsWeights = NULL, id = NULL,
            ties_method = "last",
+           # Not used
            method = "pearson",
            ...) {
+    SuperLearner:::.SL.require("weights")
     listp <- apply(X, 2, function(x, Y, method) {
-        ifelse(var(x) <= 0, 1, cor.test(x, y = Y, method = method)$p.value)
+        ifelse(var(x) <= 0, 1,
+               weights::wtd.cor(x, Y, weight = obsWeights)[1, "p.value"])
     }, Y = Y, method = method)
     return(rank(listp, ties.method = ties_method))
 }
